99a5626d68638136ca3cee2812309fa1adc37016
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-04-02 06:52:58 +0100

    Project Setup, added dependencies, worked on directories and layouts...

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className='layout'>
+            <span>Hi, I am Faidah Alao</span>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Assets/Index.js
@@ -0,0 +1,7 @@
+import fai from "./Images/fai.png"
+import mountains from "./Images/mountains.png"
+import planets from "./Images/planets.png"
+import stars from "./Images/stars.png"
+import sun from "./Images/sun.png"
+
+export { fai, mountains, planets, stars, sun }
