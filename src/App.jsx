d0dbebae9a2e46954941e659304747a866d996c0
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-04-02 07:04:11 +0100

    Added Components...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import { About, BackToTop, Contact, Hero, Navbar, Projects, Qualification, Services, Skills } from "./Components/Index"
+
 const App = () => {
     return (
         <div className='layout'>
-            <span>Hi, I am Faidah Alao</span>
+            <header>
+                <Navbar />
+            </header>
+            <main>
+                <span>Hi, I am Faidah Alao</span>
+                <Hero />
+                <About />
+                <Skills />
+                <Services />
+                <Qualification />
+                <Projects />
+                <Contact />
+            </main>
+            <footer>
+                <BackToTop />
+            </footer>
         </div>
     )
 }
---
+++ src/Components/Index.js
@@ -0,0 +1,11 @@
+import About from "./About";
+import BackToTop from "./BackToTop";
+import Contact from "./Contact";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+import Qualification from "./Qualification";
+import Services from "./Services";
+import Skills from "./Skills";
+
+export { About, BackToTop, Contact, Hero, Navbar, Projects, Qualification, Services, Skills }
