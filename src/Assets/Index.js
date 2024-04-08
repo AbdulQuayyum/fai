46b2acd9fcfb77c9f1c5288e865ea6195d663fca
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-04-08 12:40:34 +0100

    Added some images...

--- src/Assets/Index.js
+++ src/Assets/Index.js
@@ -1,3 +1,6 @@
 import fai from "./Images/fai.jpeg"
+import fai01 from "./Images/fai1.jpg"
+import fai02 from "./Images/fai2.jpg"
+import fai03 from "./Images/fai3.jpg"
 
-export { fai }
+export { fai, fai01, fai02, fai03 }
