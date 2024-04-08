95775414956b02839cf949b406950be7c12c668d
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-04-08 12:23:36 +0100

    removed unsused images...

--- src/Assets/Index.js
+++ src/Assets/Index.js
@@ -1,7 +1,3 @@
 import fai from "./Images/fai.jpeg"
-import mountains from "./Images/mountains.png"
-import planets from "./Images/planets.png"
-import stars from "./Images/stars.png"
-import sun from "./Images/sun.png"
 
-export { fai, mountains, planets, stars, sun }
+export { fai }
